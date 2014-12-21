610b57fe3747341f586a754db5a05ea553d1af1f
Colin Walters <walters@verbum.org>
2014-12-20 21:35:21 -0500

    variant: Generate type string, validate it
    In order to get decent coverage, include a format string in the fuzzed
    input.  But we need to validate it before really using it, since
    invalid format strings are a programmer error.

--- glib-parse-variant.c
+++ glib-parse-variant.c
@@ -1,12 +1,15 @@
 #include <glib.h>
+#include <string.h>
 
 int
 main (int argc, char **argv)
 {
   const char *fname = argv[1];
   const char *fmt;
+  int r;
   char *buf;
-  gsize len;
+  char *aligned_buf;
+  gsize len, vlen;
   GError *error = NULL;
   GVariant *container;
   GVariant *vay;
@@ -18,12 +21,27 @@ main (int argc, char **argv)
   container = g_variant_new_from_data (G_VARIANT_TYPE ("(say)"), (guint8*)buf, len,
 				       FALSE, NULL, NULL);
   g_variant_get (container, "(&s@ay)", &fmt, &vay);
-  g_print ("Loading: %s\n", fmt);
-  v = g_variant_new_from_data (G_VARIANT_TYPE (fmt),
-			       g_variant_get_data (vay),
-			       g_variant_get_size (vay),
-			       FALSE, NULL, NULL);
 
+  if (!g_variant_type_string_is_valid (fmt))
+    {
+      g_printerr ("Invalid format string: %s\n", fmt);
+      return 1;
+    }
+  if (!g_variant_type_is_definite ((GVariantType*)fmt))
+    {
+      g_printerr ("Indefinite format string: %s\n", fmt);
+      return 1;
+    }
+
+  g_print ("Loading: %s %p\n", fmt, (gpointer)vay);
+  vlen = g_variant_get_size (vay);
+  r = posix_memalign (&aligned_buf, sizeof(void*), vlen);
+  g_assert (r == 0);
+  memcpy (aligned_buf, g_variant_get_data (vay), vlen);
+  v = g_variant_new_from_data (G_VARIANT_TYPE (fmt),
+			       aligned_buf,
+			       vlen,
+			       FALSE, NULL, NULL);
   (void) g_variant_print (v, TRUE);
   (void) g_variant_print (v, FALSE);
   
